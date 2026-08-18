4c3be3a2a9514ac9aa2eaee18c5cf4c9ae8d318d
MaranyThea <thea.marany@gmail.com>
2026-08-18 13:51:36 +0700

    Add authentication guard and enhance dashboard layout with navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,22 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
   },
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-  },
+{
+  path: 'dashboard',
+  component: DashboardComponent,
+  canActivate: [authGuard],
+},
   {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full',
   },
+
 ];
