b6e4f56c0a8bcc4de95b53e9728e8a6c59f5b4d7
MaranyThea <thea.marany@gmail.com>
2026-08-18 16:36:13 +0700

    Refactor projects component: implement project listing and styling

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { ProjectService } from '../../services/project.service';
+import { Project } from '../../models/project';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
 
+  private readonly projectService = inject(ProjectService);
+
+  projects: Project[] = [];
+
+  ngOnInit(): void {
+    this.projectService.projects$.subscribe(projects => {
+      this.projects = projects;
+    });
+  }
 }
